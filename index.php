ec96acae31ec69f061ede031e9330a89e517ead9
Oamogetswe Mgidi <oamotebogomgidi@gmail.com>
2022-09-22 16:20:09 +0200

    Added a modal and a button

--- index.php
+++ index.php
@@ -8,7 +8,35 @@
     <title>Bootstrap PHP Modal CRUD</title>
 </head>
 <body>
-    
+    <!-- Modal -->
+<div class="modal fade" id="completeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="form-group">
+        <label for="completename">Name</label>
+        <input type="email" class="form-control" id="completename" placeholder="Enter your name">
+    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container my-3">
+    <h1 class="text-center">PHP CRUD operations using Bootstrap Modal</h1>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#completeModal">Add New user</button>
+</div>
+
 <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
