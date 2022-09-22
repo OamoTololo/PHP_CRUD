e0fe7e78180afb37e7b128d1b010fc9ac7e9811f
Oamogetswe Mgidi <oamotebogomgidi@gmail.com>
2022-09-22 16:44:52 +0200

    Modified the modal buttons.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">New User</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -37,8 +37,8 @@
     </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-dark" data-dismiss="modal">Submit</button>
+        <button type="button" class="btn btn-danger">Close</button>
       </div>
     </div>
   </div>
